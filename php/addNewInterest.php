723bf079677d7d7314c6ae86b067a0dadde8bec7
Laghava <laghava@ubuntu.(none)>
2012-11-27 21:33:24 +0530

    The first working website

---
+++ php/addNewInterest.php
@@ -0,0 +1,101 @@
+<?php
+function redirect_to_home() {
+	header( 'Location: http://www.laghava.com/index.html');
+	exit();
+}
+function redirect_to_success() {
+	header( 'Location: http://www.laghava.com/addedNewUser.html');
+	exit();
+}
+/**
+Validate an email address.
+Provide email address (raw input)
+Returns true if the email address has the email 
+address format and the domain exists.
+*/
+function validEmail($email)
+{
+   $isValid = true;
+   $atIndex = strrpos($email, "@");
+   if (is_bool($atIndex) && !$atIndex)
+   {
+      $isValid = false;
+   }
+   else
+   {
+      $domain = substr($email, $atIndex+1);
+      $local = substr($email, 0, $atIndex);
+      $localLen = strlen($local);
+      $domainLen = strlen($domain);
+      if ($localLen < 1 || $localLen > 64)
+      {
+         // local part length exceeded
+         $isValid = false;
+      }
+      else if ($domainLen < 1 || $domainLen > 255)
+      {
+         // domain part length exceeded
+         $isValid = false;
+      }
+      else if ($local[0] == '.' || $local[$localLen-1] == '.')
+      {
+         // local part starts or ends with '.'
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $local))
+      {
+         // local part has two consecutive dots
+         $isValid = false;
+      }
+      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
+      {
+         // character not valid in domain part
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $domain))
+      {
+         // domain part has two consecutive dots
+         $isValid = false;
+      }
+      else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+                 str_replace("\\\\","",$local)))
+      {
+         // character not valid in local part unless 
+         // local part is quoted
+         if (!preg_match('/^"(\\\\"|[^"])+"$/',
+             str_replace("\\\\","",$local)))
+         {
+            $isValid = false;
+         }
+      }
+      if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A")))
+      {
+         // domain not found in DNS
+         $isValid = false;
+      }
+   }
+   return $isValid;
+}
+
+
+
+$value = $_POST["emailaddress"];
+
+if (strlen($value) > 100) {
+	error_log("input value too long");
+	redirect_to_home();
+}
+
+if (!validEmail($value)) {
+	error_log("invalid email");
+	redirect_to_home();
+}
+$link = mysql_connect('laghuserdata.db.10041875.hostedresource.com','laghuserdata','Lagh@va123');
+if (!$link) {
+	error_log("Could not connect to mysql");
+	redirect_to_home();
+}
+mysql_select_db("laghuserdata",$link);
+mysql_query("insert into interestedUsers value ('$value');",$link);
+mysql_close($link);
+redirect_to_success();
