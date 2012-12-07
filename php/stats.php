d411e94070a41cf9ddfac5bea6c8921339c23309
Laghava <laghava@ubuntu.(none)>
2012-12-07 14:57:38 +0530

    stats_update_event populates the db wrongly

--- php/stats.php
+++ php/stats.php
@@ -11,7 +11,7 @@ if (!$link) {
 	die();
 }
 mysql_select_db("laghuserdata",$link);
-$result = mysql_query("insert into userevents values  (NOW(),'".$event."','".session_id()."','".substr($_SERVER['HTTP_USER_AGENT'],0,40)."','".$c1."','".$c2."','".$c3."','".$c4."','".$c5."','".$d1."','".$d2."','".$d3."','".$d4."','".$d5."');",$link);
+$result = mysql_query("insert into userevents values  (NOW(),'".session_id()."','".$event."','".substr($_SERVER['HTTP_USER_AGENT'],0,40)."','".$c1."','".$c2."','".$c3."','".$c4."','".$c5."','".$d1."','".$d2."','".$d3."','".$d4."','".$d5."');",$link);
 if (!$result) {
     error_log('Invalid query: ' . mysql_error());
     die();
