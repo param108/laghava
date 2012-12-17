a6f323369bfec285b2049bc770f0f5badc3eeae0
Laghava <laghava@ubuntu.(none)>
2012-12-17 13:06:32 +0530

    resetting previous survey.php

--- survey.php
+++ survey.php
@@ -4,7 +4,6 @@
 	include_once("php/stats.php");
 	if (!check_if_spider()) {
 		session_start();
-		$_SESSION["laghstate"]="entersurvey";
 		stats_update_event("HIT_SURVEY_ENTRY");
 	}
 ?>
@@ -13,34 +12,48 @@
   <head>
     <script src="js/jquery-latest.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script>
+	function hideMobileCheck(){
+		$(".mobilecheck").hide();
+	}
+    </script>
     <title>lAghavA:lightness of being</title>
     <!-- Bootstrap -->
-    <link href="css/checkboxes.css" rel="stylesheet" media="screen"/>
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+    <link href="css/checkboxes.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
   </head>
   <body>
-    <div style="color:azure; background-image:url('img/titlebg.png');background-repeat:repeat-x;display:block;width:100%;top:0;">
-      <div style="padding-top:5px;padding-bottom:5px;margin-left:auto;margin-right:auto;width:70%;height:10%">
+<?php
+	if ($md->isMobile()) {
+		print('<div class="mobilecheck" style="color:khaki;background-color:#245982;width:100%;line-height:50px;position:fixed;font-size:x-large;">Do you want the mobile version?<br>
+	  <a  class="btn input-block-level btn-primary" style="font-size:x-large;line-height:50px;" href="surveycontinue.php"><strong>Yes</strong> switch to mobile version</a><br>
+	  <span  class="btn input-block-level btn-primary" style="font-size:x-large;line-height:50px;" onClick="hideMobileCheck()"><strong>No</strong> do not switch</span>
+	</div>');
+	}
+?>
+    <div style="background-image:url('img/design.png');width:100%;height:100%;position:fixed;z-index:-1;overflow:scroll;">
+    <div style="color:azure; background-image:url('img/titlebg.png');background-repeat:repeat-x;display:block;width:100%;height:10%;top:0;">
+      <div style="margin:auto;margin-left:auto;margin-right:auto;width:70%;height:10%">
 	<div style="height:100%;">
-	  <b style="font-size:x-large;height:100%;"><a style="color:black;" href="index.php">lAghavA:</a></b><i style="color:azure;">lightness of Being</i>
+	  <b style="line-height:50px;font-size:30px;height:100%;"><a style="color:black;" href="index.php">lAghavA:</a></b><i style="color:azure;font-size:20px;">lightness of Being</i>
 	</div>
       </div>
     </div>
-    <br/>
-    <div style="margin-left:auto;margin-right:auto;width:95%;text-align:center;">
-    <span class="needbgnd" style="color:khaki;display:inline-block;font-size:large;text-align:center;"><strong>This survey is an anonymous survey. We do not store any of your personal data other than the answers that you choose.</strong><br/><br/></span><br/><br/>
+    <div class="needbgnd" style="margin-left:auto;margin-right:auto;width:70%;text-align:center;">
+	<i style="color:khaki"><strong>This survey is an anonymous survey. We do not store any of your personal data other than the answers that you choose.</strong></i><br><br>
     </div>
-    <div style="margin-left:auto;margin-right:auto;width:95%;text-align:center;">
+    <div style="margin-left:auto;margin-right:auto;width:70%;margin-top:10%;text-align:center;">
       <span class="needbgnd" style="display:inline-block;">
-	<p class="lead" style="font-size:large">Are you a resident of Bangalore?</p>
+      <p class="lead" style="font-size:25px">Are you a resident of Bangalore?</p>
 	<p style="text-align:left;list-style-type:none;width:100%;">
-	  <a  class="btn input-block-level btn-primary" style="font-size:large;" href="surveycontinue.php"><strong>Yes</strong>, I reside in Bangalore</a><br/><br/>
-	    <a  class="btn input-block-level btn-primary" style="font-size:large;" href="SurveyNotNow.php"><strong>No</strong>, I dont reside in Bangalore</a>
+	  <a  class="btn input-block-level btn-primary" href="surveycontinue.php"><strong>Yes</strong>, I reside in Bangalore</a><br>
+	  <a  class="btn input-block-level btn-primary" href="SurveyNotNow.php"><strong>No</strong>, I dont reside in Bangalore</a>
 	</p>
       </span>
     </div>    
-    <div style="width:95%;text-align:center;z-index:-1;">
-      <strong style="font-size:x-large;">Interested in taking your life to the next level? Why not <a class="btn btn-primary" href="willcall.php">take us for a spin</a> ?</strong>
+    <div style="width:100%;position:absolute;top:90%;text-align:center;z-index:-1;">
+      <strong style="font-size:25px;">Interested in taking your life to the next level? Why not <a class="btn btn-primary" href="willcall.php">take us for a spin</a> ?</strong>
     </div>
+  </div>
   </body>
 </html>
