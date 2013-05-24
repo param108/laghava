e1a4a8cc6f757a7089533393233a18e6facca83f
Paramananda Ponnaiyan <pponnaiyan@ubuntu.(none)>
2013-05-23 21:41:20 -0700

    Adding Timestamp to logs

--- php/debug.php
+++ php/debug.php
@@ -1,5 +1,11 @@
 <?php
+function _getDateString() {
+	$date = new DateTime();
+	$dateStr = '[' . date_format($date, 'Y-m-d H:i:s') . ',' . date_format($date, 'O') . ']';
+	return $dateStr;
+}
+
 function mylog($str) {
-	error_log($str."\n",3,"logfile.txt");
+	error_log(_getDateString() . ' ' . $str."\n",3,"logfile.txt");
 }
 
